6684a5ffccf18eaad43f92393924c0a19c05b26b
Evan W Wood <47158539+ew414716@users.noreply.github.com>
2019-02-18 21:53:36 -0500

    Week5 Homework

---
+++ 3401_Week5_Wood/Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, 5))
+            Debug.Log(transform.name);
+        if (Physics.Raycast(transform.position, fwd, 5) == false)
+            Debug.Log("NOTHING AHEAD");
+    } 
+        }
+
+
